525e80761a576355348e506c80963be7c091010e
osmuntsun <osmuntsun900606@gmail.com>
2026-03-09 12:30:12 +0800

    ETL 專案，在Binance上獲取市場資料並儲存至postgreSQL，同時在airflow進行每小時抓取

---
+++ Binance/ETL_Binance.py
@@ -0,0 +1,77 @@
+import requests
+import psycopg2
+from psycopg2 import errors
+import datetime
+
+# get_klines 返回的是列表：[[timestamp, open, high, low, close, volume, ...], ...]
+def get_klines(symbol, interval, timeZone='+8:00', limit=500):
+    req = requests.Session()
+    '''
+
+    來源: https://developers.binance.com/docs/zh-CN/binance-spot-api-docs/rest-api/market-data-endpoints#k%E7%BA%BF%E6%95%B0%E6%8D%AE
+    /api/v3/klines K棒數據
+    預設抓取最近500筆資料，最多可抓取1000筆資料
+
+    參數說明
+    參數	類型	必填	說明   
+    symbol	STRING	YES	交易對，例如：BTCUSDT
+    interval	ENUM	YES	K線的時間間隔，請參考K線時間間隔說明
+    startTime	LONG	NO	K線的開始時間，單位為毫秒
+    endTime	LONG	NO	K線的結束時間，單位為毫秒
+    timeZone STRING	NO	返回的K線數據的時間戳格式，預設為UTC，支持UTC和CST +8:00是換成台灣時間
+    limit	INT	NO	返回的K線數據條數，預設為500，最大為1000
+    '''
+    palyload = {
+        'symbol': symbol,
+        'interval': interval,
+        'timeZone': timeZone,
+        'limit': limit
+    }
+    response = req.get("https://data-api.binance.vision/api/v3/klines", params=palyload).json()
+    return response
+
+def save_data_to_db(symbol,data):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="Binance_DB",
+        user="postgres",
+        password="password"
+    )
+
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO futures_market_data
+    (symbol, timestamp, open, high, low, close, volume)
+    VALUES (%s,%s,%s,%s,%s,%s,%s)
+    """
+
+    dt = datetime.datetime.fromtimestamp(data[0] / 1000)
+
+    try:
+        cursor.execute(query,(
+            symbol,
+            dt, # 開盤時間
+            data[1], # 開盤價
+            data[2], # 最高價
+            data[3], # 最低價
+            data[4], # 收盤價
+            data[5]  # 成交量
+        ))
+        conn.commit()
+    except errors.UniqueViolation:
+        print(f"已有資料: {symbol} - {dt}")
+        conn.rollback() # 發生錯誤時回滾事務，確保資料庫保持一致性
+    except Exception as e:
+        print(f"插入失敗: {e}")
+        conn.rollback() # 發生錯誤時回滾事務，確保資料庫保持一致性
+    finally:
+        cursor.close()
+        conn.close()
+    
+
+# 獲得K線數據
+data = get_klines("BTCUSDT", "1h", timeZone='+8:00', limit=200)
+# 將每筆K線數據存入資料庫，這裡 i 是每個 K 線項目，如 [timestamp, open, high, low, close, volume]
+for i in data:
+    save_data_to_db("BTCUSDT", i)
